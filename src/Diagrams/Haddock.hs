3fb371347d51c821db5da9d89b53d9ce19a1227f
Brent Yorgey <byorgey@cis.upenn.edu>
2013-02-21 14:31:49 -0500

    a few more comments

--- src/Diagrams/Haddock.hs
+++ src/Diagrams/Haddock.hs
@@ -185,7 +185,9 @@ getComment (Comment _ _ c) = c
 --   outputting final diagrams, and all the relevant code blocks,
 --   compile the diagram referenced by a single URL, returning a new
 --   URL updated to point to the location of the generated diagram.
-compileDiagram :: FilePath -> FilePath -> [CodeBlock] -> DiagramURL -> IO DiagramURL
+compileDiagram :: FilePath   -- ^ cache directory
+               -> FilePath   -- ^ output directory
+               -> [CodeBlock] -> DiagramURL -> IO DiagramURL
 compileDiagram cacheDir outputDir code url = do
   createDirectoryIfMissing True outputDir
   createDirectoryIfMissing True cacheDir
@@ -222,7 +224,9 @@ compileDiagram cacheDir outputDir code url = do
    mkCached base = cacheDir </> base <.> "svg"
 
 -- | Compile all the diagrams referenced in a single comment.
-compileComment :: FilePath -> FilePath -> [CodeBlock] -> CommentWithURLs -> IO CommentWithURLs
+compileComment :: FilePath  -- ^ cache directory
+               -> FilePath  -- ^ output directory
+               -> [CodeBlock] -> CommentWithURLs -> IO CommentWithURLs
 compileComment cacheDir outputDir code c = do
   urls' <-
     mapM (either
@@ -233,7 +237,9 @@ compileComment cacheDir outputDir code c = do
   return $ c { diagramURLs = urls' }
 
 -- | Compile all the diagrams referenced in an entire module.
-compileDiagrams :: FilePath -> FilePath -> ParsedModule -> IO ParsedModule
+compileDiagrams :: FilePath  -- ^ cache directory
+                -> FilePath  -- ^ output directory
+                -> ParsedModule -> IO ParsedModule
 compileDiagrams cacheDir outputDir m = do
   comments' <- mapM (compileComment cacheDir outputDir (pmCode m))
                     (pmComments m)
