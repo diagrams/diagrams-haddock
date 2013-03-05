169fb02de67093f538b8924b702846d0bba2086e
Brent Yorgey <byorgey@cis.upenn.edu>
2013-03-04 19:54:44 -0500

    Implement processing of Cabal packages.

--- tools/diagrams-haddock.hs
+++ tools/diagrams-haddock.hs
@@ -1,14 +1,20 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 module Main where
 
-import           Control.Monad          (forM_, when)
-import           Data.List              (intercalate)
-import           Data.Version           (showVersion)
+import           Control.Monad                      (forM_, when)
+import           Data.List                          (intercalate)
+import           Data.Version                       (showVersion)
 import           Diagrams.Haddock
 import           System.Console.CmdArgs
 import           System.Directory
+import           System.FilePath                    ((<.>), (</>))
 
-import           Paths_diagrams_haddock (version)
+import           Distribution.ModuleName            (toFilePath)
+import qualified Distribution.PackageDescription    as P
+import           Distribution.Simple.Configure      (maybeGetPersistBuildConfig)
+import           Distribution.Simple.LocalBuildInfo (localPkgDescr)
+
+import           Paths_diagrams_haddock             (version)
 
 data DiagramsHaddock
   = DiagramsHaddock
@@ -65,9 +71,44 @@ main = do
 targetError :: FilePath -> IO ()
 targetError targ = putStrLn $ "Warning: target " ++ targ ++ " does not exist, ignoring."
 
+-- | Process all the files in a Cabal package.  At the moment, only
+--   looks at files corresponding to exported modules from a library.
+--   In other words, it does not consider the source for executables
+--   or any unexported modules.
 processCabalPackage :: DiagramsHaddock -> FilePath -> IO ()
-processCabalPackage = undefined
+processCabalPackage opts dir = do
+  mlbi <- maybeGetPersistBuildConfig distDir
+  case mlbi of
+    Nothing -> putStrLn $
+      "No setup-config found in " ++ distDir ++ ", please run 'cabal configure' first."
+    Just lbi -> do
+      let mlib = P.library . localPkgDescr $ lbi
+      case mlib of
+        Nothing -> return ()
+        Just lib -> do
+          let srcDirs = P.hsSourceDirs . P.libBuildInfo $ lib
+          mapM_ (tryProcessFile opts dir srcDirs) . map toFilePath . P.exposedModules $ lib
 
+  where distDir = dir </> "dist"
+
+-- | Try to find and process a file corresponding to a module exported
+--   from a library.
+tryProcessFile
+  :: DiagramsHaddock   -- ^ options
+  -> FilePath          -- ^ base directory
+  -> [FilePath]        -- ^ haskell-src-dirs
+  -> FilePath          -- ^ name of the module to look for, in \"A/B/C\" form
+  -> IO ()
+tryProcessFile opts dir srcDirs fileBase = do
+  let files = [ dir </> srcDir </> fileBase <.> ext
+              | srcDir <- srcDirs
+              , ext <- ["hs", "lhs"]
+              ]
+  forM_ files $ \f -> do
+    e <- doesFileExist f
+    when e $ processFile opts f
+
+-- | Process a single file with diagrams-haddock.
 processFile :: DiagramsHaddock -> FilePath -> IO ()
 processFile opts file = do
   errs <- processHaddockDiagrams (cacheDir opts) (outputDir opts) file
