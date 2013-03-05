ddaf64765fbc085bddf93f894ce971c926ca5fc1
Brent Yorgey <byorgey@cis.upenn.edu>
2013-03-05 00:32:34 -0500

    make Setup.hs copy all SVG files in the diagrams dir, not just greenCircle

--- Setup.hs
+++ Setup.hs
@@ -1,9 +1,11 @@
+import           Data.List                 (isSuffixOf)
 import           Distribution.Simple
 import           Distribution.Simple.Setup (Flag (..), HaddockFlags,
                                             haddockDistPref)
 import           Distribution.Simple.Utils (copyFiles)
 import           Distribution.Text         (display)
 import           Distribution.Verbosity    (normal)
+import           System.Directory          (getDirectoryContents)
 import           System.FilePath           ((</>))
 
 -- Ugly hack, logic copied from Distribution.Simple.Haddock
@@ -16,8 +18,10 @@ haddockOutputDir flags pkg = destDir
      destDir = baseDir </> "doc" </> "html" </> display (packageName pkg)
 
 main :: IO ()
-main = defaultMainWithHooks simpleUserHooks {
-    postHaddock = \args flags pkg lbi -> do
-        copyFiles normal (haddockOutputDir flags pkg) [("", "diagrams/greenCircle.svg")]
-        postHaddock simpleUserHooks args flags pkg lbi
-  }
+main = defaultMainWithHooks simpleUserHooks
+         { postHaddock = \args flags pkg lbi -> do
+             dias <- filter ("svg" `isSuffixOf`) `fmap` getDirectoryContents "diagrams"
+             copyFiles normal (haddockOutputDir flags pkg)
+               (map (\d -> ("", "diagrams" </> d)) dias)
+             postHaddock simpleUserHooks args flags pkg lbi
+         }
