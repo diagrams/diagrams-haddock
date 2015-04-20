d2905c55419a30b1b5b287850ffb4474a249cedb
Christopher Chalmers <c.chalmers@me.com>
2015-04-20 21:13:28 +0100

    Add FlexibleContexts for ghc-7.10
    And bump to 0.3.0.1

--- src/Diagrams/Haddock.hs
+++ src/Diagrams/Haddock.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE FlexibleContexts           #-}
 {-# LANGUAGE OverloadedStrings          #-}
 {-# LANGUAGE ScopedTypeVariables        #-}
 {-# LANGUAGE TemplateHaskell            #-}
